14b167895accb6513e4ec35a3ac82189741555cb
Chris Simpkins <git.simpkins@gmail.com>
2018-02-07 21:43:59 -0500

    attempt to run tests with cmd.exe on Windows

--- gor_test.go
+++ gor_test.go
@@ -10,7 +10,7 @@ import (
 // Golang stdlib: bytes package
 func TestBytesGor(t *testing.T) {
 	testpath := filepath.Join("test", "bytes.gor")
-	response := subprocess.Run(testpath)
+	response := subprocess.RunShell("", "", testpath)
 	if response.StdOut != "true\n" {
 		t.Errorf("[FAIL] Expected response of 'true' and received response '%s'", response.StdOut)
 	}
@@ -18,7 +18,7 @@ func TestBytesGor(t *testing.T) {
 
 func TestBytes(t *testing.T) {
 	testpath := filepath.Join("test", "bytes")
-	response := subprocess.Run(testpath)
+	response := subprocess.RunShell("", "", testpath)
 	if response.StdOut != "true\n" {
 		t.Errorf("[FAIL] Expected response of 'true' and received response '%s'", response.StdOut)
 	}
