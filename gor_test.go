c34110f4c7fcac5bbd2788ae47949b2d13936605
Chris Simpkins <git.simpkins@gmail.com>
2018-02-07 21:10:28 -0500

    added Go stdlib bytes package tests

--- gor_test.go
+++ gor_test.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"testing"
+	"github.com/go-rillas/subprocess"
+)
+
+
+// Golang stdlib: bytes package
+func TestBytesGor(t *testing.T) {
+	response := subprocess.Run("test/bytes.gor")
+	if response.StdOut != "true\n" {
+		t.Errorf("[FAIL] Expected response of 'true' and received response '%s'", response.StdOut)
+	}
+}
+
+func TestBytes(t *testing.T) {
+	response := subprocess.Run("test/bytes")
+	if response.StdOut != "true\n" {
+		t.Errorf("[FAIL] Expected response of 'true' and received response '%s'", response.StdOut)
+	}
+}
