48d1414c122248f04c912e4c5b8512c1624a048d
Chris Simpkins <git.simpkins@gmail.com>
2018-02-08 21:52:11 -0500

    Added Go stdlib builtin function tests

--- gor_test.go
+++ gor_test.go
@@ -7,12 +7,31 @@ import (
 	"github.com/go-rillas/subprocess"
 )
 
+
+// Golang stdlib builtin function
+
+func TestBuiltin(t *testing.T) {
+	testpath := filepath.Join("test", "go_stdlib", "builtin")
+	response := subprocess.RunShell("", "", "gor", testpath)
+	if response.StdOut != "2" {
+		t.Errorf("[FAIL] Expected response of '2' and received response '%s'", response.StdOut)
+	}
+}
+
+func TestBuiltinGor(t *testing.T) {
+	testpath := filepath.Join("test", "go_stdlib", "builtin.gor")
+	response := subprocess.RunShell("", "", "gor", testpath)
+	if response.StdOut != "2" {
+		t.Errorf("[FAIL] Expected response of '2' and received response '%s'", response.StdOut)
+	}
+}
+
 // Golang stdlib: bytes package
 
 func TestBytes(t *testing.T) {
 	testpath := filepath.Join("test", "go_stdlib", "bytes")
 	response := subprocess.RunShell("", "", "gor", testpath)
-	if response.StdOut != "true\n" {
+	if response.StdOut != "true" {
 		t.Errorf("[FAIL] Expected response of 'true' and received response '%s'", response.StdOut)
 	}
 }
@@ -20,7 +39,9 @@ func TestBytes(t *testing.T) {
 func TestBytesGor(t *testing.T) {
 	testpath := filepath.Join("test", "go_stdlib", "bytes.gor")
 	response := subprocess.RunShell("", "", "gor", testpath)
-	if response.StdOut != "true\n" {
+	if response.StdOut != "true" {
 		t.Errorf("[FAIL] Expected response of 'true' and received response '%s'", response.StdOut)
 	}
 }
+
+
--- gor_unix_test.go
+++ gor_unix_test.go
@@ -9,12 +9,31 @@ import (
 	"github.com/go-rillas/subprocess"
 )
 
+// Golang stdlib builtin function
+
+func TestBuiltinDirectOnUnix(t *testing.T) {
+	testpath := filepath.Join("test", "go_stdlib", "builtin")
+	response := subprocess.RunShell("", "", testpath)
+	if response.StdOut != "2" {
+		t.Errorf("[FAIL] Expected response of '2' and received response '%s'", response.StdOut)
+	}
+}
+
+func TestBuiltinGorDirectOnUnix(t *testing.T) {
+	testpath := filepath.Join("test", "go_stdlib", "builtin.gor")
+	response := subprocess.RunShell("", "", testpath)
+	if response.StdOut != "2" {
+		t.Errorf("[FAIL] Expected response of '2' and received response '%s'", response.StdOut)
+	}
+}
+
+
 // Golang stdlib: bytes package
 
 func TestBytesDirectOnUnix(t *testing.T) {
 	testpath := filepath.Join("test", "go_stdlib", "bytes")
 	response := subprocess.RunShell("", "", testpath)
-	if response.StdOut != "true\n" {
+	if response.StdOut != "true" {
 		t.Errorf("[FAIL] Expected response of 'true' and received response '%s'", response.StdOut)
 	}
 }
@@ -22,7 +41,7 @@ func TestBytesDirectOnUnix(t *testing.T) {
 func TestBytesGorDirectOnUnix(t *testing.T) {
 	testpath := filepath.Join("test", "go_stdlib", "bytes.gor")
 	response := subprocess.RunShell("", "", testpath)
-	if response.StdOut != "true\n" {
+	if response.StdOut != "true" {
 		t.Errorf("[FAIL] Expected response of 'true' and received response '%s'", response.StdOut)
 	}
 }
